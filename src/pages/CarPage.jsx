bf17b2c67f687afbb8986a13dc17acd095471485
AceTM <ace@Taruns-MacBook-Air.local>
2025-01-05 20:37:14 +0530

    scrollTo

--- src/pages/CarPage.jsx
+++ src/pages/CarPage.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import CarDetail from '../components/CarDetail'
 import Footer from '../components/Footer'
+import { useEffect } from 'react'
 
 const CarPage = () => {
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scroll to the top of the page
+  }, []);
+
   return (
     <div>
       <Navbar/>
